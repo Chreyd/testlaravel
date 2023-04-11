8c32c3829bafd3aba660f1af5573631630986448
Fleudry <chreydmapeba@gmail.com>
2023-04-11 15:38:53 +0000

    les vues blade, la navigation et passage de param

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,38 +6,28 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    private $title='Mon super titre';
-
-    public function getTitle() {
-		return $this->title;
-	}
-
-	/**
-	 * @param mixed $title
-	 * @return self
-	 */
-	public function setTitle($title): self {
-		$this->title = $title;
-		return $this;
-	}
-
-
-	/**
-	 * @return mixed
-	 */
-    public function index(){
-        $posts=[
+    private $title = 'Mon super titre';
+    public function index()
+    {
+        $posts = [
             'Mon super titre 1',
             'Mon super titre Deuxième',
         ];
-        $titre1='Mon super titre 1';
-        $titre2='Mon super titre 2';
-        // return view('articles')->with('titre',$titre1);
-        // return view('articles',compact('titre1', 'titre2'));
-/*         return view('articles',[
-            'titre1'=>$titre1,
-            'titre2'=>$titre2,
-        ]); */
-        return view('articles',compact('posts'));
+
+        return view('articles', compact('posts'));
+    }
+    public function show($id){
+        $posts = [
+           1=> 'Mon super titre 1',
+           2=> 'Mon super titre Deuxième',
+        ];
+        $post= $posts[$id] ?? 'pas de titre';//terner
+        return view('article',[
+            'post'=>$post
+        ]);
+    }
+
+    public function contact(){
+        return view('contact');
     }
 }
